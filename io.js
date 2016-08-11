e54438ab724a4df83276de1c61a10c6b695a8a5e
Gustavo Ferreira <gustavogardel@gmai.com>
2016-08-11 15:42:19 -0700

    set the clear

---
+++ io.js
@@ -0,0 +1,17 @@
+// io.js
+
+var io = require('socket.io')();
+
+// Listen for new connections from clients (socket)
+io.on('connection', function (socket) {
+  console.log('Client connected to socket.io!');
+  socket.on('add-circle', function (data) {
+      io.emit('add-circle', data);
+  });
+  socket.on('clear', function (data) {
+      io.emit('clear', data);
+  });
+});
+
+// io represents socket.io on the server - let's export it
+module.exports = io;
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-//svar routes = require('./routes/index');
-
 var app = express();
 
 app.use(logger('dev'));
